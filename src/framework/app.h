9771eeb633980a0bf151c17faa8de0c4414613ea
Forairaaaaa <applesyqd@outlook.com>
2023-05-07 12:23:04 +0800

    more app api

--- src/framework/app.h
+++ src/framework/app.h
@@ -17,20 +17,33 @@ namespace MOONCAKE {
 
     /* Inherit and override "Life cycle" to create your own App */
     class APP_BASE {
-        protected:
+        private:
             std::string _name;
+            void* _user_data;
             void* _icon_addr;
             bool _allow_bg_running;
             bool _finished;
             SIMPLEKV::SimpleKV* _database;
 
+
+        protected:
             /* App internal API */
+            inline void setAppName(const std::string& name) { _name = name; }
+            inline void setAppIcon(void* icon) { _icon_addr = icon; }
             inline SIMPLEKV::SimpleKV* Database() { return _database; }
+            inline void* getUserData() { return _user_data; };
+            inline void allowBackgroudRunning(bool allow) { _allow_bg_running = allow; }
             inline void endApp() { _finished = true; }
 
 
         public:
-            APP_BASE() : _name(""), _icon_addr(nullptr), _allow_bg_running(false), _finished(false), _database(nullptr) {}
+            APP_BASE() :
+                _name(""),
+                _user_data(nullptr),
+                _icon_addr(nullptr),
+                _allow_bg_running(false),
+                _finished(false),
+                _database(nullptr) {}
             ~APP_BASE() = default;
 
 
@@ -50,6 +63,30 @@ namespace MOONCAKE {
             virtual void onRunning() {}
             virtual void onPause() {}
             virtual void onDestroy() {}
+
+    };
+
+
+    struct APPList_t {
+        APP_BASE* app = nullptr;
+        uint16_t id = 0;
+    };
+
+
+    class APP_Register {
+        private:
+            std::vector<APPList_t> _app_list;
+
+        public:
+            APP_Register() {}
+            ~APP_Register() = default;
+
+            /* Basic API */
+            bool install(APP_BASE* app, SIMPLEKV::SimpleKV* database, void* userData = nullptr);
+            bool uninstall(APP_BASE* app);
+
+            inline std::vector<APPList_t> getAppList() { return _app_list; }
+
 
     };
 
