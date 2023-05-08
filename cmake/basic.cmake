f7e64a3bd82cacec94da2bb784c6b52f53d4cd53
Forairaaaaa <applesyqd@outlook.com>
2023-05-09 07:51:04 +0800

    detach lvgl and hal

--- cmake/basic.cmake
+++ cmake/basic.cmake
@@ -2,20 +2,16 @@
 
 cmake_minimum_required(VERSION 3.10)
 set(CMAKE_C_STANDARD 11)#C11
-set(CMAKE_CXX_STANDARD 17)#C17
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 
 # Mooncake
 file(GLOB_RECURSE MOONCAKE_SRCS
-    ${MOONCAKE_ROOT_DIR}/src/hal/platform/linux_sdl/*.cpp
     ${MOONCAKE_ROOT_DIR}/src/app/*.cpp
     ${MOONCAKE_ROOT_DIR}/src/framework/*.cpp
 )
 set(MOONCAKE_INCS
     ${MOONCAKE_ROOT_DIR}/src/
-    ${MOONCAKE_ROOT_DIR}/src/hal/
-    ${MOONCAKE_ROOT_DIR}/src/hal/platform/linux_sdl/
     ${MOONCAKE_ROOT_DIR}/app/
     ${MOONCAKE_ROOT_DIR}/framework/
 )
@@ -28,18 +24,6 @@ set(SIMPLEKV_SRCS
 set(SIMPLEKV_INCS
     ${MOONCAKE_ROOT_DIR}/src/simplekv/
 )
-
-
-# SDL2
-find_package(SDL2 REQUIRED SDL2)
-include_directories(${SDL2_INCLUDE_DIRS})
-
-
-# lvgl
-set(MY_LV_CONF_PATH ${MOONCAKE_ROOT_DIR}/src/hal/platform/linux_sdl/lv_conf.h)
-add_definitions(-DLV_CONF_PATH=${MY_LV_CONF_PATH})
-add_subdirectory(${MOONCAKE_ROOT_DIR}/src/lvgl)
-
 
 
 #  Build library
@@ -57,4 +41,4 @@ target_include_directories(mooncake PUBLIC
 
 
 # Link
-target_link_libraries(mooncake PUBLIC ${SDL2_LIBRARIES} lvgl lvgl::examples lvgl::demos)
+target_link_libraries(mooncake PUBLIC lvgl lvgl::examples lvgl::demos)
